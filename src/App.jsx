99f148bbbbcaad9e09f9254bc2e61fed33bb86a5
Tameem Ulla <tameem.ulla@brillio.com>
2026-04-09 23:20:34 +0530

    Few components added along with UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
+import { TaskProvider } from "./context/TaskContext.jsx";
 import Home from "./pages/Home.jsx";
 import './App.css'
 
 function App() {
  return
  (
-    <Home />
+    <TaskProvider>
+       <Home />
+    </TaskProvider>
  );
 }
 
